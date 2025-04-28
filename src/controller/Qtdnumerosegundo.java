825b615012382f91e7f14d3e1e7f0034441145d5
lucasbezerrademacedo <lucasbezerrademacedo@192.168.33.133>
2025-04-28 14:42:31 -0300

    Segundo commit.

--- src/controller/Qtdnumerosegundo.java
+++ src/controller/Qtdnumerosegundo.java
@@ -5,14 +5,10 @@ public class Qtdnumerosegundo {
 	public Qtdnumerosegundo() {
 		super();
 	}
-	public int Qtdnumseg(int num1, int num2) {
-		if (num1 == 0) {
-			return 0; /* Condicao de parada: quando o numero for 0, nao será mais possivel 
-			dividir e todos os numeros já terao sido verificados. */
+	public int qtdN2(int n1, int n2) {
+		if (n1 == 0) {
+			return 0; 
 		}
-			return (num1 % 10 == num2 ? 1:0) + Qtdnumseg(num1/10, num2);
-			/* Se o resto de num1/10 for igual a num2, retorna 1 indicando que num2 
-			 aparece em num1, senao, retorna 0. Esse retorno, 1 ou 0, é somado a chamada da
-			 funcao para validar o proximo numero. */
+		return (n1 % 10 == n2 ? 1:0) + qtdN2(n1/10, n2);
 		}
 }
--- src/viewer/Principal.java
+++ src/viewer/Principal.java
@@ -8,8 +8,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Qtdnumerosegundo qtd = new Qtdnumerosegundo ();
-		int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Insira um número inteiro de 10 a 999999"));
-		int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Insira um número inteiro de 0 a 9"));
-		JOptionPane.showMessageDialog(null, "A quantidade de vezes que o número " + numero2 + " aparece em " + numero1 + " é: " + qtd.Qtdnumseg(numero1, numero2) + ".");
+		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Insira um número inteiro de 10 a 999999"));
+		int n2 = Integer.parseInt(JOptionPane.showInputDialog("Insira um número inteiro de 0 a 9"));
+		JOptionPane.showMessageDialog(null, "A quantidade de vezes que o número " + n2 + " aparece em " + n1 + " é: " + qtd.qtdN2(n1, n2) + ".");
 	}
 }
